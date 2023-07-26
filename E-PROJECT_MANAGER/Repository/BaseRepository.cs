a8880ce786acab22a61001e12953e174ce320efc
Trần Hoàng Việt <95629544+TranHoangVi@users.noreply.github.com>
2023-07-27 01:07:15 +0700

    CRUD ho so

---
+++ E-PROJECT_MANAGER/Repository/BaseRepository.cs
@@ -0,0 +1,185 @@
+﻿using E_PROJECT_MANAGER.Data;
+using E_PROJECT_MANAGER.DataTransferObject;
+using E_PROJECT_MANAGER.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace E_PROJECT_MANAGER.Repository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        ViewDTO<T> GetPagination(int index = 1, int size = 10);
+        public List<T> GetAllItem();
+        public DataTableReposneDTO<T> GetAll();
+        public DataTableReposneDTO<T> Filter(Expression<Func<T, bool>> filter, string columnName = "Id",
+                                                            bool columnAsc = false,
+                                                            int start = 1,
+                                                            int length = 10,
+                                                            int draw = 0);
+
+        public T GetById(int id);
+
+        public ViewDTO<T> Insert(T entity);
+        public ViewDTO<T> Update(T entity);
+        public ViewDTO<T> Delelte(T entity);
+
+        public ViewDTO<T> Save(int id, T entity);
+
+    }
+    public class BaseRepository<T> : IBaseRepository<T> where T : Base
+    {
+
+        protected ApplicationDbContext _context;
+        protected DbSet<T> _dbSet;
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public ViewDTO<T> GetPagination(int index = 1, int size = 10)
+        {
+            var result = new ViewDTO<T>();
+            // Lấy ra tất cả số lượng của T
+            var totalRows = _dbSet.Count();
+            var dataRows = _dbSet.Skip((index - 1) * size).Take(size);
+            result.totalRows = totalRows;
+            result.DataRows = dataRows.ToList();
+            result.Message = "Lấy dữ liệu phân trang thành công!!";
+            result.StatusCode = 200;
+            return result;
+        }
+        public ViewDTO<T> Delelte(T entity)
+        {
+            var result = new ViewDTO<T>();
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges();
+                result.StatusCode = 200;
+                result.Message = "Delete success!";
+            }
+            return result;
+        }
+
+        public DataTableReposneDTO<T> Filter(Expression<Func<T, bool>> filter, string columnName = "Id", bool columnAsc = false, int start = 1, int length = 10, int draw = 0)
+        {
+            var result = new DataTableReposneDTO<T>();
+
+            var dataRows = _dbSet.AsQueryable();
+
+            //Tim kiem
+            if (filter != null)
+            {
+                dataRows = dataRows.Where(filter);
+            }
+
+            //Sap xep
+            var propertyInfo = typeof(T).GetProperty(columnName);
+            var parameter = Expression.Parameter(typeof(T), "p");
+            var property = Expression.Property(parameter, propertyInfo);
+            var lambda = Expression.Lambda<Func<T, object>>(Expression.Convert(property, typeof(object)), parameter);
+            if (columnAsc == false)
+            {
+                dataRows = dataRows.OrderByDescending(lambda);
+            }
+            else
+            {
+                dataRows = dataRows.OrderBy(lambda);
+            }
+
+            //Phan trang
+            var totalRows = dataRows.Count();
+            dataRows = dataRows.Skip(start).Take(length);
+
+            result.data = dataRows.ToList();
+            result.recordsTotal = totalRows;
+            result.recordsFiltered = totalRows;
+            result.draw = draw;
+
+            return result;
+        }
+
+        public DataTableReposneDTO<T> GetAll()
+        {
+            var result = new DataTableReposneDTO<T>();
+            result.data = _dbSet.AsQueryable().ToList();
+            return result;
+        }
+
+        public List<T> GetAllItem()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            if (id > 0)
+            {
+                var result = _dbSet.Find(id);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        public ViewDTO<T> Insert(T entity)
+        {
+            var result = new ViewDTO<T>();
+            if (entity != null)
+            {
+                if (entity.Id <= 0)
+                {
+                    _dbSet.Add(entity);
+                    _context.SaveChanges();
+                    result.Message = "Bạn đã thêm mới thành công!";
+                    result.StatusCode = 200;
+                    result.Data.Add(entity);
+                }
+            }
+            return result;
+        }
+
+        public ViewDTO<T> Save(int id, T entity)
+        {
+            ViewDTO<T> result = new ViewDTO<T>();
+            if (id <= 0)
+            {
+                _dbSet.Add(entity);
+                _context.SaveChanges();
+                result.StatusCode = 200;
+                result.Message = "Them moi thanh cong!";
+            }
+            if (id > 0)
+            {
+                _dbSet.Update(entity);
+                _context.SaveChanges();
+                result.StatusCode = 200;
+                result.Message = "Cap nhat thanh cong!";
+
+            }
+
+            return result;
+        }
+
+
+        public ViewDTO<T> Update(T entity)
+        {
+            var result = new ViewDTO<T>();
+            if (entity != null)
+            {
+                if (entity.Id > 0)
+                {
+                    _dbSet.Update(entity);
+                    _context.SaveChanges();
+                    result.Message = "Bạn đã chỉnh sửa thanh cong!";
+                    result.StatusCode = 200;
+                    result.Data.Add(entity);
+                }
+            }
+            return result;
+        }
+    }
+}
